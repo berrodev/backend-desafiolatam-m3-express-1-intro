fa36bc11588ebdcdb1058fe617bb2a71e8706536
Francisco Berroeta <fsberroeta@gmail.com>
2024-12-05 16:29:20 -0300

    refactor: remove commented instructions and clean up index.js

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-/* 
-● Generar al menos dos rutas, la principal y una protegida, tal como se vio en la clase
-de Express donde se retringió el acceso a los /productos.
-● Comprobar el funcionamiento con Postman o ThunderClient de la API y generar un
-documento con pantallazos donde se muestren las pruebas de funcionamiento que
-ejecutes.
-● Puedes utilizar como referencia el código de la aplicación de la clase Introducción a
-Express. Recuerda que esta aplicación está disponible en la plataforma.
-*/
 const express = require('express');
 const fs = require('fs');
 const jwt = require('jsonwebtoken');
