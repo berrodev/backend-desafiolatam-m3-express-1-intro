8780a582b8177faeb7d7a058b46071d5f1c210db
Francisco Berroeta <fsberroeta@gmail.com>
2024-12-05 15:37:44 -0300

    feat: add .gitignore and update package.json with dependencies; initialize index.js with project instructions

---
+++ index.js
@@ -0,0 +1,12 @@
+/* 
+Deberás iniciar un nuevo proyecto en Node.
+● Instalar paquetes como Express, JWT y Nodemon.
+● Generar al menos dos rutas, la principal y una protegida, tal como se vio en la clase
+de Express donde se retringió el acceso a los /productos.
+● Comprobar el funcionamiento con Postman o ThunderClient de la API y generar un
+documento con pantallazos donde se muestren las pruebas de funcionamiento que
+ejecutes.
+● Puedes utilizar como referencia el código de la aplicación de la clase Introducción a
+Express. Recuerda que esta aplicación está disponible en la plataforma.
+*/
+const express = require('express');
