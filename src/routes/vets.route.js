f02bcf87c72e07bfad52fa4312c1054f812d7d3b
berrodev <fsberroeta @gmail.com>
2024-12-07 16:13:57 -0300

    refactor: restructure project by moving files to src directory and implementing routing for veterinary clinic management

--- src/routes/vets.route.js
+++ src/routes/vets.route.js
@@ -1,39 +1,3 @@
-const express = require('express');
-const fs = require('fs');
-const jwt = require('jsonwebtoken');
-
-const app = express();
-app.use(express.json());
-
-app.listen(3000, () => {
-  console.log('Servidor iniciado');
-});
-
-// Base de datos simulada de usuarios
-const users = [];
-
-// Middleware de autenticación
-const SECRET_KEY = '123456';
-function authenticateToken(req, res, next) {
-  const authHeader = req.headers['authorization'];
-  const token = authHeader && authHeader.split(' ')[1];
-
-  if (token == null) {
-    console.log('Token no proporcionado');
-    return res.sendStatus(401);
-  }
-
-  jwt.verify(token, SECRET_KEY, (err, user) => {
-    if (err) {
-      console.log('Error de verificación de token:', err.message);
-      return res.sendStatus(403);
-    }
-    req.user = user;
-    console.log('Token verificado para el usuario:', user.username);
-    next();
-  });
-}
-
 // Ruta para registrarse
 app.post('/register', (req, res) => {
   console.log(req.body);
