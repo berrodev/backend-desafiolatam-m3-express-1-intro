7c94aaa05b957ebb1561dbc2626804f16e5f180c
Francisco Berroeta <fsberroeta@gmail.com>
2025-02-21 22:28:37 -0300

    feat: enhance chat functionality with user login and message handling

--- src/sockets/socket.handler.js
+++ src/sockets/socket.handler.js
@@ -1,11 +1,32 @@
 // Socket handler for chat application
 
-export default (io) => {
+const users = {};
+
+export default function (io) {
   io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('chat', (msg) => {
-      console.log(`${socket.id}: ${msg}`);
-      io.emit('chat', `${socket.id}: ${msg}`);
+    console.log('User connected', socket.id);
+
+    // User login
+    socket.on('login', (username) => {
+      users[socket.id] = username;
+      socket.emit('message', `Welcome, ${username}!`);
+    });
+
+    // Chat message
+    socket.on('chatMessage', (msg) => {
+      const username = users[socket.id] || 'Anonymous';
+      io.emit('message', `${username}: ${msg}`);
+    });
+
+    // User disconnected
+    socket.on('disconnect', () => {
+      console.log('User disconnected', users[socket.id]);
+      delete users[socket.id];
+    });
+
+    // Error handling
+    socket.on('error', (err) => {
+      console.error('Socket error:', err);
     });
   });
-};
+}
